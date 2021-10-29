3220af6bd5805179efe4ecd68fe7f7747d3a1e2d
Sam Winslow <sammywinslow@gmail.com>
2021-10-28 20:10:37 -0400

    working example of frontend compilation; run npx webpack serve

---
+++ wasm-web/src/App.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react'
+import initSwc, { transformSync } from '@swc/wasm-web'
+
+export default function App() {
+  const [initialized, setInitialized] = useState(false)
+
+  useEffect(() => {
+    async function importAndRunSwcOnMount() {
+      await initSwc()
+      setInitialized(true)
+    }
+    importAndRunSwcOnMount()
+  }, [])
+
+  function compile() {
+    if (!initialized) {
+      return
+    }
+    const result = transformSync(`console.log('hello')`, {})
+    console.log(result)
+  }
+
+  return (
+    <div className="App">
+      <button onClick={compile}>Compile</button>
+    </div>
+  )
+}
